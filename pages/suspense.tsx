fdc669ef6cc27952b28bba6c20655be23a0a723d
Leah Lundqvist <leah@pigeon.sh>
2023-09-14 14:55:17 +0200

    demo: move suspense to its own page

--- pages/suspense.tsx
+++ pages/suspense.tsx
@@ -1,18 +1,17 @@
 import React, { Fragment, Suspense } from "react"
 import Counter from "../components/counter"
 import { Link } from "../framework/client/router"
+import { Delayed } from "../components/delayed"
+import { Time } from "../components/time"
 
 export async function Page() {
-	const timeBeforeSleep = new Date()
-	await new Promise((resolve) => setTimeout(resolve, 100))
-	const timeAfterSleep = new Date()
 	return (
 		<Fragment>
 			<h1>Hello world!</h1>
-			<p>Time before sleep: {timeBeforeSleep.toISOString()}</p>
-			<p>Time after sleep: {timeAfterSleep.toISOString()}</p>
-			<Counter />
 			<br />
+			<Suspense fallback={<p>Suspense loading...</p>}>
+				<Delayed delay={1000} Component={Time} />
+			</Suspense>
 			<Link href="/subdir">go to /subdir</Link>
 		</Fragment>
 	)
